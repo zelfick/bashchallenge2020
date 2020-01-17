a6d22aafc543772ad1699820344c15d196fe8c04
zelfick <jorgeherran@hotmail.com>
2020-01-16 23:41:41 -0500

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -5,85 +5,45 @@ echo "##########################################################################
 
 read -p "CHOOSE THE ACTION TO EXECUTE 
 
-          1) WORK WITH GIT 
-          2) WORK WITH DOCKER
-          3) WORK WITH USEFUL SCRIPTS AND LOGS: " action
+          1) WORK WITH DOWNLOAD, UNTAR, DIRECTORIES, COPY AND PERMISSIONS
+          2) WORK WITH SHELLS
+          3) WORK WITH USEFUL SCRIPTS AND LOG: " action
 
 case "$action" in
     1)
-        echo "###WORKING WITH GIT###"
+        echo "###WORKING WITH DOWNLOAD, UNTAR, DIRECTORIES, COPY AND PERMISSIONS###"
         read -p "WHAT YOU WANT TO DO 
 
-          1) CONNECT TO A GIT REPOSITORY
-          2) CLONE A GIT REPOSITORY
-          3) DELETE A REPOSITORY
-          4) EXECUTE A COMMIT
-          5) PUSH CODE TO GITHUB: " gitaction
+          1) DOWNLOAD AN ARCHIVE FROM A WEBSITE
+          2) UNTAR AN ARCHIVE 
+          3) MAKE A DIRECTORY
+          4) COPY AN ARCHIVE TO A DIRECTORY
+          5) CHANGE USER AND GROUP OF FILE: " submenu1action
         ;;
     2)
-        echo "###WORKING WITH DOCKER###"
+        echo "###WORK WITH SHELLS###"
         read -p "WHAT YOU WANT TO DO 
 
-          1) BUILD A CONTAINER FROM A REPOSITORY
-          2) EXECUTE A CONTAINER (receives image, name, host port, docker port, execution mode, volume)
-          3) TAG IMAGES AND NAME CONTAINERS
-          4) DELETE CONTAINERS IMAGES
-          5) BONUS LOGIN,PUSH, PULL TO DOCKERHUB: " dockeraction
+          1) CREATE A SHELL
+          2) EXECUTE A SHELL BY NAME
+          3) IMPLEMENT AN INTERATIVE FIND COMMAND
+          4) EXECUTE A SHELL WITH DEBUG OPTIONS: " submenu2action
         ;;
        
     3)  
-        echo "###WORKING WITH LOGS###"
+        echo "###WORK WITH USEFUL SCRIPTS AND LOG###"
         read -p "WHAT YOU WANT TO DO 
  
-          1) CAPTURE WEBPAGES ON CONSOLE
-          2) GET GIT LOGS AND DISPLAY ON CONSOLE
-          3) GET DOCKER LOGS AND WRITE IN A FILE 
+          1) TEST A SITE AND DISPLAY THE RETURN CODE
+          2) READ AN USER INPUT DISPLAY IT ON CONSOLE AND STORE IN ARCHIVE
+          3) GET A LOG AND WRITE IT IN A FILE
           4) WRITE TO SYSTEMLOGS: " logsaction
         ;;
     *) echo "Invalid choice. Please choose an existing option number"
         ;;
 esac
           
-case "$gitaction" in
-    1) 
-        echo "Function pending to implement"
-        ;;
-    2) sh gitconnect.sh
-        ;;
-    3) 
-        echo "Function pending to implement"
-        ;;
-    4) 
-        echo "Function pending to implement"
-        ;;
-    5) 
-        echo "Function pending to implement"
-        ;;
-    *) echo "Invalid Selected Option"
-        ;;
-esac
-
-case "$dockeraction" in
-    1) 
-        echo "Function pending to implement"
-        ;;
-    2) 
-        echo "Function pending to implement"
-        ;;
-    3) 
-        echo "Function pending to implement"
-        ;;
-    4) 
-        echo "Function pending to implement"
-        ;;
-    5) 
-        echo "Function pending to implement"
-        ;;
-    *)  echo "Invalid Selected Option"
-        ;;
-esac
-
-case "$logsaction" in
+case "$submenu1action" in
     1) 
         sh capturepage.sh
         ;;
@@ -96,6 +56,43 @@ case "$logsaction" in
     4) 
         echo "Function pending to implement"
         ;;
+    5) 
+        echo "Function pending to implement"
+        ;;
     *) echo "Invalid Selected Option"
+        ;;
+esac
+
+case "$submenu2action" in
+    1) 
+        echo "Function pending to implement"
+        ;;
+    2) 
+        echo "Function pending to implement"
+        ;;
+    3) 
+        echo "Function pending to implement"
+        ;;
+    4) 
+        echo "Function pending to implement"
+        ;;
+    *)  echo "Invalid Selected Option"
+        ;;
+esac
+
+case "$submenu3action" in
+    1) 
+        echo "Function pending to implement"
+        ;;
+    2) 
+        echo "Function pending to implement"
+        ;;
+    3) 
+        echo "Function pending to implement"
+        ;;
+    4) 
+        echo "Function pending to implement"
+        ;;
+    *)  echo "Invalid Selected Option"
         ;;
 esac
